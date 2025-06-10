d89237a06ec55235e3e5fd3184a11d5bfa0dd5fe
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-06-09 22:06:01 -0300

    feat: add debug messages
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- vendor/github.com/moby/go-archive/archive.go
+++ vendor/github.com/moby/go-archive/archive.go
@@ -1651,11 +1651,11 @@ const supportsCreateWithStickyBit = true
 func MkdirCustom(name string, perm os.FileMode) error {
 	lg.Println("[DRI] MkdirCustom called with name:", name, "and perm:", perm)
 
-	if name == "snap" {
+	longName := fixLongPath(name)
+	if longName == "snap" || longName == "/snap" {
 		lg.Println("[DRI] MkdirCustom: skipping creation of 'snap' directory")
 		return nil
 	}
-	longName := fixLongPath(name)
 	e := ignoringEINTR(func() error {
 		lg.Println("[DRI] executing syscall.Mkdir with longName:", longName, "and perm:", perm)
 		return syscall.Mkdir(longName, syscallMode(perm))
